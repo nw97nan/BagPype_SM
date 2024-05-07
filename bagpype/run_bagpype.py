0bbec0ade1c508432712c0b0286ea962efde7142
Wilson <wilsonweiwu@gmail.com>
2024-05-07 20:52:05 +0100

    Restructured run_bagpype to a class structure with init from mol/mol2 file and directories options

--- bagpype/run_bagpype.py
+++ bagpype/run_bagpype.py
@@ -28,82 +28,99 @@ logger.addHandler(fh)
 logger.addHandler(sh)
 
 
-def run_bagpype(
-    mol_id,
-    mol_file,
-    mol2_file,
-    output_dir='./',
-    strip={'res_name': [], 'chain': [], 'residues': [],},
-    trim_H=True,
-    add_H=True,
-    MakeMultimer_number=None,
-):
+class RunBagPype:
+    def __init__(
+            self,
+            output_dir='output/',           # Dir for all outputs to be saved in
+            del_output=True,
+    ) -> None:
 
-    import bagpype
+        # Define paths
+        self.output_dir = create_dir(check_dirpath(output_dir), replace=del_output)     # Create output dir if not exists
 
-    generate_cif_het = create_files.Ligand_generation(
-        mol_file=mol_file,
-        mol2_file=mol2_file,
-    )
 
-    generate_cif_het.cif_writer('./bagpype/dependencies/mmcif/NWU.cif')
-    generate_cif_het.HET_writer('./bagpype/dependencies/NWU.txt')
+    def init_from_mol_file(self, mol_file, mol2_file):
+        self.mol_files = [mol_file]
+        self.mol2_files = [mol2_file]
 
-    mol_dir = create_dir(output_dir, f'{mol_id}/')
 
-    pdb_file = mol_dir + mol_id + '.pdb'
-    generate_cif_het.pdb_writer(pdb_file)
+    def init_from_mol_dir(self, mol_dir, mol2_dir):
+        self.mol_files = glob.glob(check_dirpath(mol_dir)+'*.mol')
+        self.mol2_files = glob.glob(check_dirpath(mol2_dir)+'*.mol2')
 
-    importlib.reload(bagpype)
 
-    myprot = bagpype.molecules.Protein()
-    parser = bagpype.parsing.PDBParser(pdb_file)
+    def create_bagpype_pdb(self):
 
-    parser.parse(
-        myprot,
-        strip=strip,
-        trim_H=trim_H,
-        add_H=add_H,
-        MakeMultimer_number=MakeMultimer_number,
-    )
+        get_id = lambda file : pathlib.Path(file).stem.split('.')[0]
 
-    ggenerator = bagpype.construction.Graph_constructor()
+        mol_dict = {get_id(file): file for file in self.mol_files}
+        mol2_dict = {get_id(file): file for file in self.mol2_files}
 
-    atoms_file_name = mol_dir + mol_id + '_atoms.csv'
-    bonds_file_name = mol_dir + mol_id + '_bonds.csv'
+        # Setup tqdm tracking bar
+        pbar = tqdm(total=len(mol_dict))
 
-    ggenerator.construct_graph(
-        myprot,
+        for mol_id in mol_dict.keys():
+
+            pbar.set_description(f'Working on {mol_id}')
+
+            self.run_bagpype(
+                mol_id=mol_id,
+                mol_file=mol_dict[mol_id],
+                mol2_file=mol2_dict[mol_id],
+            )
+
+            pbar.update()
+        
+        pbar.close()
+
+
+    def run_bagpype(
+        self,
         mol_id,
-        atoms_file_name=atoms_file_name,
-        bonds_file_name=bonds_file_name,
-    )
+        mol_file,
+        mol2_file,
+        strip={'res_name': [], 'chain': [], 'residues': [],},
+        trim_H=True,
+        add_H=True,
+        MakeMultimer_number=None,
+    ):
 
+        import bagpype
 
-def batch_run_bagpype(mol_dir, mol2_dir, output_dir):
-
-    mol_files = glob.glob(check_dirpath(mol_dir)+'*.mol')
-    mol2_files = glob.glob(check_dirpath(mol2_dir)+'*.mol2')
-
-    get_id = lambda file : pathlib.Path(file).stem.split('.')[0]
-
-    mol_dict = {get_id(file): file for file in mol_files}
-    mol2_dict = {get_id(file): file for file in mol2_files}
-
-    # Setup tqdm tracking bar
-    pbar = tqdm(total=len(mol_dict))
-
-    for mol_id in mol_dict.keys():
-
-        pbar.set_description(f'Working on {mol_id}')
-
-        run_bagpype(
-            mol_id=mol_id,
-            mol_file=mol_dict[mol_id],
-            mol2_file=mol2_dict[mol_id],
-            output_dir=output_dir,
+        generate_cif_het = create_files.Ligand_generation(
+            mol_file=mol_file,
+            mol2_file=mol2_file,
         )
 
-        pbar.update()
-    
-    pbar.close()
+        generate_cif_het.cif_writer('./bagpype/dependencies/mmcif/NWU.cif')
+        generate_cif_het.HET_writer('./bagpype/dependencies/NWU.txt')
+
+        mol_dir = create_dir(self.output_dir, f'{mol_id}/')
+
+        pdb_file = mol_dir + mol_id + '.pdb'
+        generate_cif_het.pdb_writer(pdb_file)
+
+        importlib.reload(bagpype)
+
+        myprot = bagpype.molecules.Protein()
+        parser = bagpype.parsing.PDBParser(pdb_file)
+
+        parser.parse(
+            myprot,
+            strip=strip,
+            trim_H=trim_H,
+            add_H=add_H,
+            MakeMultimer_number=MakeMultimer_number,
+        )
+
+        ggenerator = bagpype.construction.Graph_constructor()
+
+        atoms_file_name = mol_dir + mol_id + '_atoms.csv'
+        bonds_file_name = mol_dir + mol_id + '_bonds.csv'
+
+        ggenerator.construct_graph(
+            myprot,
+            mol_id,
+            atoms_file_name=atoms_file_name,
+            bonds_file_name=bonds_file_name,
+        )
